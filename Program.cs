522aa5edb9dfb36d90766ce1aa01e29001212ed2
Артём Минидогов <agario.po@mail.ru>
2022-02-08 18:43:29 +0200

    git

--- Program.cs
+++ Program.cs
@@ -11,14 +11,103 @@ namespace Kordamine_Esimene_C
         
         static void Main(string[] args)
         {
-            Console.WriteLine("a= ");
+            Console.WriteLine("Kas sa tahad midagi osta? (1/2)");
+            int ans = int.Parse(Console.ReadLine());
+            if (ans==1)
+            {
+                Console.WriteLine("Poodis on piim,sai ja leib");
+                Console.WriteLine("Kas sa soovib osta piima ? Hind:1.13 => ");
+                string piim = Console.ReadLine();
+                if (piim.ToUpper()=="JAH")
+                {
+                    Console.WriteLine("Added...");
+                    Console.WriteLine("Kas sa soovib osta saia ? Hind:0.99 => ");
+                    string sai = Console.ReadLine();
+                    if (sai.ToUpper()=="JAH")
+                    {
+                        Console.WriteLine("Added...");
+                        Console.WriteLine("Kas sa soovib osta leiba ? Hind:0.73 => ");
+                        string leib = Console.ReadLine();
+                        if (leib.ToUpper()=="JAH")
+                        {
+                            Console.WriteLine("Added...");
+                            float w = (float)(1.13 + 0.99 + 0.73);
+                            Console.WriteLine("Price {0} euro", w);
+                            Console.ReadKey();
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Remove...");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Remove...");
+                }
+
+
+            }
+            else
+            {
+                Console.WriteLine("Viga...");
+                return;
+            }
+            /*Console.WriteLine("Mis pikkus sa oled? => ");
+            float pikk = float.Parse(Console.ReadLine());
+            Console.WriteLine("Sa oled naine või mees? (mees=1)(naine=2)");
+            int pol = int.Parse(Console.ReadLine());
+            if (pikk<=160)
+            {
+                Console.WriteLine("Sa oled lühike.");
+            }
+            else if (pikk < 160 || pikk > 190 )
+            {
+                Console.WriteLine("Sa oled keskmine.");
+            }
+            else if (pikk>=190)
+            {
+                Console.WriteLine("Sa oled pikk.");
+            }
+            Console.ReadKey();
+            /*Console.WriteLine("a= ");
             int a = int.Parse(Console.ReadLine());
             Console.WriteLine("b= ");
             int b = int.Parse(Console.ReadLine());
-            int S;
-            S = a * b;
-            Console.WriteLine("Pindala",S);
-            Console.ReadKey();
+            int S = a * b;
+            Console.WriteLine("Pindala = {0}",S);
+            Console.WriteLine("Kas soovite tegeda romondi ? => ");
+            string remont = Console.ReadLine();
+            if (remont.ToUpper() == "JAH" || remont.ToUpper() == "YES")
+            {
+                Console.WriteLine("Kui palju maksab üks ruutmeeter ? =>");
+                int c = int.Parse(Console.ReadLine());
+                int Ruut = S * c;
+                Console.WriteLine("Põranda vahetamise hind => {0}", Ruut);
+                float skid = (float)(Ruut * 0.3);
+                int skid1 = (int)(Ruut - skid);
+                Console.WriteLine("30% hinnasoodustusega => {0}\nHind koos hinnasoodustusega => {1}", skid,skid1);
+                    Console.WriteLine("Kas temperatuur kodus on üle kaheksateistkümne kraadi (18) => (jah/ei)");
+                    string temp = Console.ReadLine();
+                    if (temp.ToUpper()=="JAH")
+                    {
+                        Console.WriteLine("Hästi,see on soovitav toasoojus talvel temperatuur.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Again...");
+                        return;
+                    }
+                    Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Viga");
+                return;
+            }
+            Console.ReadLine();
+
+
             /*Console.WriteLine("Tere nimeta nime pinginaabri => ");
             string name1 = Console.ReadLine();
             Console.WriteLine("Nimeta nime pinginaabri => ");
